279628a659e2299dc1f1b13ef9ddd67137db0320
bvv94 <bvdevasconcelos@gmail.com>
2022-11-21 12:32:13 -0400

    usando useState para alterar perfil OK

--- src/Components/Usuario.js
+++ src/Components/Usuario.js
@@ -1,32 +1,27 @@
-
+import React from "react";
 
 export default function Usuario() {
-    let nomedefault = "Catana";
-    let imgperfil = "./assets/catanacomics.svg";
-    // let imgperfil = "https://img1.gratispng.com/20180401/goe/kisspng-user-profile-computer-icons-profile-5ac09244d91906.2547020615225697968892.jpg";
+const [nome, setNome] = React.useState("Catana");
+const [img, setImg] = React.useState("./assets/catanacomics.svg");
+
+// let imgperfil = "https://img1.gratispng.com/20180401/goe/kisspng-user-profile-computer-icons-profile-5ac09244d91906.2547020615225697968892.jpg";
 
     function novoNome() {
-        console.log(`entrou novoNome, nome default ${nomedefault}`);
-        nomedefault = prompt("editar nome: ")
-        console.log(`Mudou novoNome, nome agora ${nomedefault}`);
-        // Usuario(); AINDA NÃO TÔ CONSEGUINDO IMPRIMIR NA TELA
+        setNome (prompt("Informe o novo nome: "));  
     }
 
     function novaImagem() {
-        console.log(`entrou novaImagem, img default ${imgperfil}`);
-        imgperfil = prompt("Informe o link para nova imagem: ");
-        console.log(`Mudou novaImagem, img agora ${imgperfil}`);
-        // Usuario(); AINDA NÃO TÔ CONSEGUINDO IMPRIMIR NA TELA
+        setImg (prompt ("Informe o link da nova imagem: "))
     }
 
     return (
         <div class="perfil" data-test="user">
             <div class="foto">
-                <img onClick={novaImagem} class="circulo" src={imgperfil} data-test="profile-image" />
+                <img onClick={novaImagem} class="circulo" src={img} data-test="profile-image" />
             </div>
             <div class="usuario">
                 <div class="nomepublico">catanacomics</div>
-                <div class="nomeusuario" data-test="name">{nomedefault}<span  data-test="edit-name" onClick={novoNome}><ion-icon name="pencil-outline"></ion-icon></span></div>
+                <div class="nomeusuario" data-test="name">{nome}<span onClick={novoNome} data-test="edit-name"><ion-icon name="pencil-outline"></ion-icon></span></div>
             </div>
         </div>
     )
